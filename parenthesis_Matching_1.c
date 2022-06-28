4bb5b67c693f331a961e99f8e87479efa93e65d5
Taduri Lokesh <88146458+LokeshTaduri1@users.noreply.github.com>
2022-06-28 13:54:02 +0530

    Create parenthesis_Matching_1.c

---
+++ parenthesis_Matching_1.c
@@ -0,0 +1,88 @@
+/* Parenthesis Matching using Stack */
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    char data;
+    struct node *next;
+}*top=NULL;
+void push(char x)
+{
+    struct node *t;
+    t=(struct node *)malloc(sizeof(struct node));
+    t->data=x;
+    t->next=top;
+    top=t;
+}
+void pop()
+{
+    struct node *t;
+    t=top;
+    top=top->next;
+    free(t);
+}
+void display()
+{
+    struct node *p=top;
+    while(p!=NULL)
+    {
+        printf("%d ",p->data);
+        p=p->next;
+    }
+}
+
+int isbalance(string exp)
+{
+    int x=0;
+    bool flag= true;
+    for(int i=0;exp[i]!='\0';i++)
+    {
+        if(exp[i]=='('||exp[i]=='{'||exp[i]=='[')  
+        {
+            push(exp[i]);
+        }
+        if(exp[i]==')'){
+           struct node *t = top; 
+           if(top!=NULL && t->data=='('){
+               pop();
+           }
+           else{
+            flag = false;
+            break;
+           }
+        }
+        if(exp[i]=='}'){
+           struct node *t = top; 
+           if(top!=NULL && t->data=='{'){
+               pop();
+           }
+           else{
+            flag = false;
+            break;
+           }
+        }
+        else if(exp[i]==']'){
+           struct node *t = top; 
+           if(top!=NULL && t->data=='['){
+               pop();
+           }
+           else{
+            flag = false;
+            break;
+           }
+        }
+
+    }
+    if(top!=NULL) return 0;
+    if(flag==false) return 0;
+    else return 1;
+}
+
+int main()
+{
+    string exp="([{(a+b)*(b+c)}])";
+    printf("%d",isbalance(exp));
+}
